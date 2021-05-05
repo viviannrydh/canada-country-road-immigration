ee0ce5494245b1123282bab3df88d2a1d6313c2f
Viviann Rydh <rydh.vivian@gmail.com>
2021-05-05 10:05:45 +0200

    fixed title icon and title name

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,24 +1,8 @@
 import React from 'react';
 import googlemap from '../pictures/googlemap.png';
 import QR from '../pictures/QR.jpeg'
-import { useState } from 'react';
-
-
 
 const Contact = () => {
-    const [userName, setUserName]=useState('');
-    const [userCity, setUserCity]=useState('');
-    const [userTel, setUserTel]=useState('');
-    const [userEmail, setUserEmail]=useState('');
-    const [userMessage, setUserMessage]=useState('');
-    
-    const handleChange=(e)=>{
-        setUserName(e.target.value);
-        setUserCity(e.target.value);
-        setUserTel(e.target.value);
-        setUserEmail(e.target.value);
-        setUserMessage(e.target.value)
-    }
 
     const handleSubmit=(e)=>{
         e.preventDefault();
@@ -50,23 +34,23 @@ const Contact = () => {
                             </div>
                             <div className="input-item">
                                 <p>姓名: </p>
-                                <input type="text" name="userName" onChange={handleChange}/>
+                                <input type="text" name="userName" />
                             </div>
                             <div className="input-item">
                                 <p>您所在的城市: </p>
-                                <input type="text" name="userCity" onChange={handleChange}/>
+                                <input type="text" name="userCity" />
                             </div>
                             <div className="input-item">
                                 <p>您的电话: </p>
-                                <input type="text" name="userTel" onChange={handleChange}/>
+                                <input type="text" name="userTel" />
                             </div>
                             <div className="input-item">
                                 <p>您的邮箱: </p>
-                                <input type="text" name="userEmail" onChange={handleChange}/>
+                                <input type="text" name="userEmail" />
                             </div>
                             <div className="input-area">
                                 <p>需要帮助的问题: </p>
-                                <textarea type="text" name="userMessage" onChange={handleChange}/>
+                                <textarea type="text" name="userMessage" />
                             </div>
                             <button className='submit-button' type="submit">提交</button>
                </form>
